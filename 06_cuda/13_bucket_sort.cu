950faf39b6672cb13845ddc359d802abc722e852
UN-L <hyper1960@outlook.fr>
2024-05-14 05:10:44 +0200

    CUDA homework

---
+++ 06_cuda/13_bucket_sort.cu
@@ -0,0 +1,50 @@
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+
+__global__ void bucketSort(int range, int n, int bucket[], int key[], int offset[])
+{
+  int i = threadIdx.x;
+  for (int j = 1; j<range; j<<=1) {
+    offset[i] = bucket[i];
+    if(i>=j) bucket[i] += offset[i-j];
+  }
+
+  for (int j=0; bucket[i]>0; bucket[i]--)
+    key[j++] = i;
+}
+
+int main() 
+{
+  int n = 50;
+  int range = 5;
+  int *key;
+  cudaMallocManaged(&key, range*sizeof(int));
+
+  for (int i=0; i<n; i++) {
+    key[i] = rand() % range;
+    printf("%d ",key[i]);
+  }
+
+  printf("\n");
+
+  int *bucket;
+  cudaMallocManaged(&bucket, range*sizeof(int));
+  int *offset;
+  cudaMallocManaged(&offset, range*sizeof(int)); 
+  for (int i=0; i<range; i++) {
+    bucket[i] = 0;
+  }
+  for (int i=0; i<n; i++) {
+    bucket[key[i]]++;
+  }
+  bucketSort<<<1, range>>>(range, n, bucket, key, offset);
+  cudaDeviceSynchronize();
+
+  for (int i=0; i<n; i++) {
+    printf("%d ",key[i]);
+  }
+  printf("\n");
+  cudaFree(key);
+  cudaFree(bucket);
+}
