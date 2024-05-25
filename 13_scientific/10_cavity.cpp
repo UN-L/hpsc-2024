06cd254ab7ceabb86692b3989dbbe865d5a63742
UN-L <hyper1960@outlook.fr>
2024-05-25 17:11:42 +0200

    Final report: added 10_cavity.cpp for python code translation and 10_cavity.cu for CUDA

---
+++ 13_scientific/10_cavity.cpp
@@ -0,0 +1,98 @@
+#include <cstdio>
+#include <vector>
+#include <random>
+#include <iostream>
+using namespace std;
+
+int main (int argc, char** argv) {
+    int nx = 41;
+    int ny = 41;
+    int nt = 500;
+    int nit = 50;
+    double dx = 2.0 / (nx - 1);
+    double dy = 2.0 / (ny - 1);
+    float dt = .01;
+    int rho = 1;
+    float nu = .02;
+    
+    std::vector<std::vector<double>> u(ny, std::vector<double>(nx, 0.0));
+    std::vector<std::vector<double>> v(ny, std::vector<double>(nx, 0.0));
+    std::vector<std::vector<double>> p(ny, std::vector<double>(nx, 0.0));
+    std::vector<std::vector<double>> b(ny, std::vector<double>(nx, 0.0));
+
+    for (int n = 0; n < nt; ++n) 
+    {
+        for (int j = 1; j < ny - 1; ++j) 
+        {
+            for (int i = 1; i < nx - 1; ++i) 
+            {
+                b[j][i] = rho * (1 / dt *
+                (((u[j][i + 1] - u[j][i - 1]) / (2 * dx)) + ((v[j + 1][i] - v[j - 1][i]) / (2 * dy))) -
+                std::pow(((u[j][i + 1] - u[j][i - 1]) / (2 * dx)), 2) - 
+                2 * (((u[j + 1][i] - u[j - 1][i]) / (2 * dy)) * ((v[j][i + 1] - v[j][i - 1]) / (2 * dx))) - 
+                std::pow(((v[j + 1][i] - v[j - 1][i]) / (2 * dy)), 2));
+            }
+        }
+        for (int it = 0; it < nit; ++it) {
+            std::vector<std::vector<double>> pn = p;
+            for (int j = 1; j < ny - 1; ++j) {
+                for (int i = 1; i < nx - 1; ++i) {
+                    p[j][i] = (dy * dy * (pn[j][i + 1] + pn[j][i - 1]) +
+                               dx * dx * (pn[j + 1][i] + pn[j - 1][i]) -
+                               b[j][i] * dx * dx * dy * dy) /
+                              (2 * (dx * dx + dy * dy));
+                }
+            }
+            
+            //p[:, -1] = p[:, -2]
+            for (int j = 0; j < ny; ++j) p[j][nx - 1] = p[j][nx - 2];
+            //p[0, :] = p[1, :]
+            for (int i = 0; i < nx; ++i) p[0][i] = p[1][i];
+            //p[:, 0] = p[:, 1]
+            for (int j = 0; j < ny; ++j) p[j][0] = p[j][1];
+            //p[-1, :] = 0
+            for (int i = 0; i < nx; ++i) p[ny - 1][i] = 0.0;
+        }
+
+        std::vector<std::vector<double>> un = u;
+        std::vector<std::vector<double>> vn = v;
+        for (int j = 1; j < ny - 1; ++j) {
+            for (int i = 1; i < nx - 1; ++i) {
+                u[j][i] = un[j][i] - un[j][i] * dt / dx * (un[j][i] - un[j][i - 1]) -
+                        un[j][i] * dt / dy * (un[j][i] - un[j - 1][i]) -
+                        dt / (2 * rho * dx) * (p[j][i + 1] - p[j][i - 1]) +
+                        nu * dt / (dx * dx) * (un[j][i + 1] - 2 * un[j][i] + un[j][i - 1]) +
+                        nu * dt / (dy * dy) * (un[j + 1][i] - 2 * un[j][i] + un[j - 1][i]);
+                v[j][i] = vn[j][i] - vn[j][i] * dt / dx * (vn[j][i] - vn[j][i - 1]) -
+                        vn[j][i] * dt / dy * (vn[j][i] - vn[j - 1][i]) -
+                        dt / (2 * rho * dx) * (p[j + 1][i] - p[j - 1][i]) +
+                        nu * dt / (dx * dx) * (vn[j][i + 1] - 2 * vn[j][i] + vn[j][i - 1]) +
+                        nu * dt / (dy * dy) * (vn[j + 1][i] - 2 * vn[j][i] + vn[j - 1][i]);
+            }
+        }
+        //u[0, :]  = 0
+        //u[-1, :] = 1
+        //v[0, :]  = 0 
+        //v[-1, :] = 0
+        for (int i = 0; i < nx; ++i)
+        {
+            u[0][i] = 0;
+            u[nx-1][i] = 1;
+            v[0][i] = 0;
+            v[nx-1][i] = 0;
+        }
+        //u[:, 0]  = 0
+        //u[:, -1] = 0
+        //v[:, 0]  = 0
+        //v[:, -1] = 0
+        for (int j = 0; j < ny; ++j)
+        {
+            u[j][0] = 0;
+            u[j][ny-1] = 0;
+            v[j][0] = 0;
+            v[j][ny-1] = 0;
+        }
+
+    }
+    return 0;
+}
---
+++ 13_scientific/10_cavity.cu
@@ -0,0 +1,212 @@
+#include <cstdio>
+#include <vector>
+#include <random>
+#include <iostream>
+#include <curand_kernel.h>
+using namespace std;
+
+
+
+/*__global__ void linspace(double start, double end, curandState *state,double res[]) {
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    
+    double random_value = curand_uniform_double(&state[idx]);
+    res[idx] = start + random_value * (end - start);
+}*/
+__global__ void zeros(double *array, int nx, int ny) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    int index = i * ny + j;
+    if (i < nx && j < ny) {
+        array[index] = 0;
+    }
+}
+
+__global__ void compute_b(double *b, double *u, double *v, int nx, int ny, int rho, double dt, double dx, double dy) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i >= 1 && i < nx - 1 && j >= 1 && j < ny - 1) {
+        int index = j * nx + i;
+        int idx_i_plus_one = j * nx + (i+1);
+        int idx_i_minus_one = j * nx + (i-1);
+        int idx_j_plus_one = (j+1) * nx + i;
+        int idx_j_minus_one = (j-1) * nx + i;
+
+        b[index] = rho * (1 / dt *
+            (((u[idx_i_plus_one] - u[idx_i_minus_one]) / (2 * dx)) + ((v[idx_j_plus_one] - v[idx_j_minus_one]) / (2 * dy))) -
+            pow(((u[idx_i_plus_one] - u[idx_i_minus_one]) / (2 * dx)), 2) - 
+            2 * (((u[idx_j_plus_one] - u[idx_j_minus_one]) / (2 * dy)) * ((v[idx_i_plus_one] - v[idx_i_minus_one]) / (2 * dx))) - 
+            pow(((v[idx_j_plus_one] - v[idx_j_minus_one]) / (2 * dy)), 2));
+    }
+}
+
+__global__ void compute_p(double *p, double *b, double *pn, int nx, int ny, double dx, double dy) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i >= 1 && i < nx - 1 && j >= 1 && j < ny - 1) {
+        int index = j * nx + i;
+        int idx_i_plus_one = j * nx + (i+1);
+        int idx_i_minus_one = j * nx + (i-1);
+        int idx_j_plus_one = (j+1) * nx + i;
+        int idx_j_minus_one = (j-1) * nx + i;
+        p[index] = (dy * dy * (pn[idx_i_plus_one] + pn[idx_i_minus_one]) +
+                    dx * dx * (pn[idx_j_plus_one] + pn[idx_j_minus_one]) -
+                    b[index] * dx * dx * dy * dy) /
+                    (2 * (dx * dx + dy * dy));
+    }
+}
+
+__global__ void compute_uv(double *u, double *v, double *p, double *un, double *vn, int nx, int ny, int rho, double dt, double nu, double dx, double dy) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i >= 1 && i < nx - 1 && j >= 1 && j < ny - 1) {
+        int index = j * nx + i;
+        int idx_i_plus_one = j * nx + (i+1);
+        int idx_i_minus_one = j * nx + (i-1);
+        int idx_j_plus_one = (j+1) * nx + i;
+        int idx_j_minus_one = (j-1) * nx + i;
+        u[index] = un[index] - un[index] * dt / dx * (un[index] - un[idx_i_minus_one]) -
+                un[index] * dt / dy * (un[index] - un[idx_j_minus_one]) -
+                dt / (2 * rho * dx) * (p[idx_i_plus_one] - p[idx_i_minus_one]) +
+                nu * dt / (dx * dx) * (un[idx_i_plus_one] - 2 * un[index] + un[idx_i_minus_one]) +
+                nu * dt / (dy * dy) * (un[idx_j_plus_one] - 2 * un[index] + un[idx_j_minus_one]);
+        v[index] = vn[index] - vn[index] * dt / dx * (vn[index] - vn[idx_i_minus_one]) -
+                vn[index] * dt / dy * (vn[index] - vn[idx_j_minus_one]) -
+                dt / (2 * rho * dx) * (p[idx_j_plus_one] - p[idx_j_minus_one]) +
+                nu * dt / (dx * dx) * (vn[idx_i_plus_one] - 2 * vn[index] + vn[idx_i_minus_one]) +
+                nu * dt / (dy * dy) * (vn[idx_j_plus_one] - 2 * vn[index] + vn[idx_j_minus_one]);
+    }
+}
+
+__global__ void compute_pj(double *p, int nx, int ny) {
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (j < ny) {
+        int idx_minus_1 = j * nx + (nx - 1);
+        int idx_minus_2 = j * nx + (nx - 2);
+        int idx_0 = j * nx + 0;
+        int idx_1 = j * nx + 1;
+        p[idx_minus_1] = p[idx_minus_2];
+        p[idx_0] = p[idx_1];
+    }
+}
+
+__global__ void compute_pi(double *p, int nx, int ny) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < nx) {
+        int idx_minus_1 = (ny - 1) * nx + i;
+        int idx_0 = 0 * nx + i;
+        int idx_1 = 1 * nx + i;
+        p[idx_0] = p[idx_1];
+        p[idx_minus_1] = 0.0;
+    }
+}
+
+__global__ void compute_uvi(double *u, double *v, int nx, int ny) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < nx) {
+        int idx_minus_1 = (ny - 1) * nx + i;
+        int idx_0 = 0 * nx + i;
+        u[idx_0] = 0;
+        u[idx_minus_1] = 1;
+        v[idx_0] = 0;
+        v[idx_minus_1] = 0;
+    }
+}
+
+__global__ void compute_uvj(double *u, double *v, int nx, int ny) {
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (j < ny) {
+        int idx_minus_1 = j * nx + (nx - 1);
+        int idx_0 = j * nx + 0;
+        u[idx_0] = 0;
+        u[idx_minus_1] = 0;
+        v[idx_0] = 0;
+        v[idx_minus_1] = 0;
+    }
+}
+
+int main(int argc, char** argv) {
+    int nx = 41;
+    int ny = 41;
+    int nt = 500;
+    int nit = 50;
+    double dx = 2.0 / (nx - 1);
+    double dy = 2.0 / (ny - 1);
+    float dt = .01;
+    int rho = 1;
+    float nu = .02;
+
+    dim3 threadsPerBlock(16, 16);
+    dim3 numBlocks((nx + threadsPerBlock.x - 1) / threadsPerBlock.x, (ny + threadsPerBlock.y - 1) / threadsPerBlock.y);
+
+    double *u;
+    cudaMallocManaged(&u, ny*nx*sizeof(double));
+    zeros<<<numBlocks, threadsPerBlock>>>(u, nx, ny);
+    cudaDeviceSynchronize();
+
+    double *v;
+    cudaMallocManaged(&v, ny*nx*sizeof(double));
+    zeros<<<numBlocks, threadsPerBlock>>>(v, nx, ny);
+    cudaDeviceSynchronize();
+
+    double *p;
+    cudaMallocManaged(&p, ny*nx*sizeof(double));
+    zeros<<<numBlocks, threadsPerBlock>>>(p, nx, ny);
+    cudaDeviceSynchronize();
+
+    double *b;
+    cudaMallocManaged(&b, ny*nx*sizeof(double));
+    zeros<<<numBlocks, threadsPerBlock>>>(b, nx, ny);
+    cudaDeviceSynchronize();
+
+    double *pn;
+    cudaMallocManaged(&pn, ny*nx*sizeof(double));
+
+    double *un;
+    cudaMallocManaged(&un, ny*nx*sizeof(double));
+
+    double *vn;
+    cudaMallocManaged(&vn, ny*nx*sizeof(double));
+
+    for (int n = 0; n < nt; ++n) {
+        compute_b<<<numBlocks, threadsPerBlock>>>(b, u, v, nx, ny, rho, dt, dx, dy);
+        cudaDeviceSynchronize();
+
+        for (int it = 0; it < nit; ++it) {
+            cudaMemcpy(pn, p, nx * ny * sizeof(double), cudaMemcpyDeviceToDevice);
+            compute_p<<<numBlocks, threadsPerBlock>>>(p, b, pn, nx, ny, dx, dy);
+            cudaDeviceSynchronize();
+            compute_pj<<<numBlocks, threadsPerBlock>>>(p, nx, ny);
+            compute_pi<<<numBlocks, threadsPerBlock>>>(p, nx, ny);
+            cudaDeviceSynchronize();
+        }
+
+        cudaMemcpy(un, u, nx * ny * sizeof(double), cudaMemcpyDeviceToDevice);
+        cudaMemcpy(vn, v, nx * ny * sizeof(double), cudaMemcpyDeviceToDevice);
+        compute_uv<<<numBlocks, threadsPerBlock>>>(u, v, p, un, vn, nx, ny, rho, dt, nu, dx, dy);
+        cudaDeviceSynchronize();
+
+        compute_uvi<<<numBlocks, threadsPerBlock>>>(u, v, nx, ny);
+        cudaDeviceSynchronize();
+        compute_uvj<<<numBlocks, threadsPerBlock>>>(u, v, nx, ny);
+        cudaDeviceSynchronize();
+    }
+
+    std::cout << "u:\n";
+    for (int i = 0; i < ny; i++) {
+        for (int j = 0; j < nx; j++) {
+            std::cout << u[i * nx + j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    cudaFree(u);
+    cudaFree(v);
+    cudaFree(p);
+    cudaFree(b);
+    cudaFree(pn);
+    cudaFree(un);
+    cudaFree(vn);
+
+    return 0;
+}
